a381a09791278a5ea15a6effe462121194fcae09
Mark Wilkes <mcwilkes@gmail.com>
2025-03-02 12:04:18 -0700

    add a few of my scripts and their aliases

---
+++ mr.py
@@ -0,0 +1,24 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description="Process some file.")
+parser.add_argument('file', type=str, help='The file name with extension')
+args = parser.parse_args()
+
+file_name = args.file
+name = file_name.split('.')[0]
+ext = file_name.split('.')[-1]
+
+match ext:
+    case "py":
+        os.system("python3 {0}".format(file_name))
+    case "go":
+        os.system("go run {0}".format(file_name))
+    case "rs":
+        os.system("rustc {0}".format(file_name))
+        os.system("./{0}".format(name))
+    case "java":
+        os.system("javac {0}".format(file_name))
+        os.system("java {0}".format(name))
+    case _:
+        print("File was not .py, .go, .rs or .java!")
